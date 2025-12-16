d3e9b4b9a54d4026e677edb238ee11bd6cbbfc2a
nargesbazanjani2025-coder <nargesbazanjani2025@gmail.com>
2025-12-16 18:28:27 +0330

    paint Graphic things
    use turtle to draw a lot of wonderful things

---
+++ doreh morroor.py
@@ -0,0 +1,23 @@
+#import turtle
+#t=turtle.Pen()
+#t.reset()
+#for X in range(1,19):
+ #   t.fd(100)
+  #  if X%2==0:
+   #     t.left(175)
+    #else:
+     #   t.left(225)
+
+
+
+
+
+
+import turtle
+t=turtle.Pen()
+t.reset()
+for X in range(1,38):
+    t.color(0,1,0)
+    t.fd(100)
+    t.left(175)
+    
